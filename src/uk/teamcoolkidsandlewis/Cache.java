8234d301d1b27d62ee29ade1268b4f6a27f07d54
JRIngram <jr-ingram@hotmail.co.uk>
2017-02-23 20:31:31 +0000

    W H Y A M I S T I L L H E R E

--- src/uk/teamcoolkidsandlewis/Cache.java
+++ src/uk/teamcoolkidsandlewis/Cache.java
@@ -4,10 +4,30 @@ import java.util.ArrayList;
 
 public class Cache {
 	
-	private ArrayList<EndPoint> endpoints;
+	private ArrayList<EndPointCacheConnection> endpoints;
 	private int size; 
 	
-	public Cache(ArrayList<EndPoint> endpoints){
+	public Cache(ArrayList<EndPointCacheConnection> endpoints){
 		this.endpoints = endpoints;
+	}
+	
+	public ArrayList<EndPointCacheConnection> getEndPointCacheConnections(){
+		return endpoints;
+	}
+
+	public ArrayList<EndPointCacheConnection> getEndpoints() {
+		return endpoints;
+	}
+
+	public void setEndpoints(ArrayList<EndPointCacheConnection> endpoints) {
+		this.endpoints = endpoints;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
 	}
 }
